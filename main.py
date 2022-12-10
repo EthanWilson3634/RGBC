3faeaa5cbd709c4b00ec995ce208cd52a078bb65
Ethan Wilson <ethanwilson008@gmail.com>
2022-12-10 15:44:11 +0000

    Initializing workspace ready to define backlight method

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import time
+from rpi_ws281x import PixelStrip, Color
+import argparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# LED strip configuration:
+LED_COUNT = 238      # Number of LED pixels.
+LED_PIN = 18         # GPIO pin connected to the pixels (18 uses PWM!).
+LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
+LED_CHANNEL = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# Optional configurations skipped:
+# LED_FREQ_HZ, LED_DMA, LED_INVERT
 
+# Define LED backlight method here
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+    
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_BRIGHTNESS, LED_CHANNEL)
+    # Initialize the library
+    strip.begin()
+    
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+        
+    try:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        while True:
+            # Call LED backlight method here
+
+    except KeyboardInterrupt:
+        if args.clear:
+            colorWipe(strip, Color(0, 0, 0), 10)
